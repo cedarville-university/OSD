0589556e4aea0b0a2ed35b6aa6e54dffcaa26bfb
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2023-10-16 14:56:56 -0400

    Add files via upload

---
+++ Remove-OneDriveSetup_RunKey.ps1
@@ -0,0 +1,20 @@
+﻿#Create PSDrive for HKU
+New-PSDrive -PSProvider Registry -Name HKUDefaultHive -Root HKEY_USERS
+
+#Load Default User Hive
+reg load "HKU\DefaultHive" "C:\Users\Default\NTUSER.DAT"
+
+#Set OneDriveSetup Variable
+$oneDriveSetup = Get-ItemProperty "HKUDefaultHive:\DefaultHive\Software\Microsoft\Windows\CurrentVersion\Run" | select -ExpandProperty "OneDriveSetup"
+
+#If Variable returns True, remove OneDriveSetup Value
+If ($oneDriveSetup) { Remove-ItemProperty -Path "HKUDefaultHive:\DefaultHive\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDriveSetup"}
+
+#Unload Hive
+Reg Unload "HKU\DefaultHive\"
+
+#Remove PSDrive HKUDefaultHive
+Remove-PSDrive "HKUDefaultHive"
+
+#Remove OneDrive Installation
+Start-Process -FilePath "C:\Windows\System32\OneDriveSetup.exe" -ArgumentList '/uninstall' -Wait
