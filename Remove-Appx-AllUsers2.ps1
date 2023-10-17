2115c5e27d882a19a2f249e6d0dd3bf62f36a8cb
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2023-10-17 16:08:49 -0400

    Add files via upload

---
+++ Remove-Appx-AllUsers2.ps1
@@ -0,0 +1,31 @@
+﻿# Define an array of package names for the applications you want to remove
+$packageNames = @(
+    "Microsoft.549981C3F5F10",
+    "Microsoft.BingNews",
+    "Microsoft.BingWeather",
+    "Microsoft.GamingApp",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.WindowsCommunicationsApps",
+    "Microsoft.GetHelp",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.People",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo",
+    "MicrosoftTeams"
+)
+
+# Loop through the array and remove the specified applications
+foreach ($packageName in $packageNames) {
+    if (Get-AppxPackage -Name $packageName -AllUsers) {
+        Get-AppxPackage -Name $packageName -AllUsers | Remove-AppxPackage -AllUsers
+        Write-Host "Application $packageName has been removed."
+    } else {
+        Write-Host "Application $packageName is not installed."
+    }
+}
