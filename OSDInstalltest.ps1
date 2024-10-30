0cf33057cca88d5a9eec9d92e3c4d2967acee63c
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2024-10-30 16:37:03 -0400

    Update OSDInstalltest.ps1

--- OSDInstalltest.ps1
+++ OSDInstalltest.ps1
@@ -1,1 +1,13 @@
+#================================================
+#   [PreOS] Update Module 
+#================================================
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
 
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force -SkipPublisherCheck
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force   
