e6855029912c46f8b45f0db4b39a1db50432d98d
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2024-11-01 15:14:23 -0400

    Update OSDInstalltest.ps1

--- OSDInstalltest.ps1
+++ OSDInstalltest.ps1
@@ -12,11 +12,12 @@ Install-Module OSD -Force -SkipPublisherCheck
 Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force  
 
+# Set Bios Settings for Dell Systems
 if((Get-MyComputerManufacturer) -match "Dell Inc.") {
     Write-Host "Setting Dell Bios Settings"
     x:\osdcloud\Config\Dell\BiosSetting\Set-CUDellBiosSettings.ps1
 }
-
+# Set Bios Settings for Lenovo Systems
 if((Get-MyComputerManufacturer) -match "LENOVO") {
     Write-Host "Setting Lenovo Bios Settings"
     x:\osdcloud\Config\Lenovo\BiosSetting\Set-LenovoBiosSettings.ps1
