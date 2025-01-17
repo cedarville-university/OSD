d6b2db0c4aa7bb7c807424a7fa3e8c7e6daf5f8b
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2025-01-17 15:57:51 -0500

    Update OSDtesting.ps1

--- OSDtesting.ps1
+++ OSDtesting.ps1
@@ -6,8 +6,6 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Set-DisRes 1600
 }
 
-
-
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
 Install-Module OSD -Force -SkipPublisherCheck
 
@@ -27,34 +25,6 @@ $Params = @{
 }
 Start-OSDCloud @Params
 
-Copy-Item -Path "X:\OSDCloud\Config\RegScripts\AutopilotEnrollment.ps1" -Destination "c:\OSDCloud\Temp\" -Force
-Copy-Item -Path "X:\OSDCloud\Config\Scripts\Copy-Unattendxml1.ps1" -Destination "C:\OSDCloud\Temp" -Force
-& "C:\OSDCloud\Temp\Copy-UnattendXML1.ps1"
-
-pause
-
-#================================================
-#  [PostOS] OOBEDeploy Configuration
-#================================================
-Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json"
-$OOBEDeployJson = @'
-{
-    "Autopilot":  {
-                      "IsPresent":  false
-                  },
-    "UpdateDrivers":  {
-                          "IsPresent":  true
-                      },
-    "UpdateWindows":  {
-                          "IsPresent":  true
-                      }
-}
-'@
-If (!(Test-Path "C:\ProgramData\OSDeploy")) {
-    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
-}
-$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
-
 #================================================
 #  [PostOS] AutopilotOOBE CMD Command Line
 #================================================
@@ -62,9 +32,9 @@ Write-Host -ForegroundColor Green "Create C:\Windows\System32\Autopilot.cmd"
 $AutopilotCMD = @'
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
+Start /wait Powershell -NoL -C Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose -SkipPublisherCheck
-Start /Wait PowerShell -NoL -C Install-Module AutoPilotOOBE -Force -Verbose -SkipPublisherCheck
-Start /Wait PowerShell -NoL -C Start-OOBEDeploy
+Start /Wait PowerShell -NoL -C Start-OOBEDeploy -UpdateDrivers -UpdateWindows
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/SystemPrepScript.ps1
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/Remove-Appx-AllUsers2.ps1
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/Remove-OneDriveSetup_RunKey.ps1
@@ -88,5 +58,4 @@ $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.c
 #=======================================================================
 Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
-pause
 wpeutil reboot
