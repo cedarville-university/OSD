2165a011353722e7ee199b39d16893bb7dc1288b
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2025-02-06 15:58:45 -0500

    Update OSDInstall.ps1

--- OSDInstall.ps1
+++ OSDInstall.ps1
@@ -35,7 +35,6 @@ Start /wait Powershell -NoL -C [Net.ServicePointManager]::SecurityProtocol = [Ne
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /wait Powershell -NoL -C Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose -SkipPublisherCheck
-Start /Wait PowerShell -NoL -C Start-WindowsUpdateDriver
 Start /Wait PowerShell -NoL -C Start-WindowsUpdate
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/SystemPrepScript.ps1
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/Remove-Appx-AllUsers2.ps1
