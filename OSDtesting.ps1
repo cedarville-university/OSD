771d593729179abf0c6e0e39509afafab5a2f4c8
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2025-02-12 13:30:14 -0500

    Update OSDtesting.ps1

--- OSDtesting.ps1
+++ OSDtesting.ps1
@@ -35,11 +35,8 @@ Start /wait Powershell -NoL -C [Net.ServicePointManager]::SecurityProtocol = [Ne
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /wait Powershell -NoL -C Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose -SkipPublisherCheck
-Start /Wait PowerShell -NoL -C Start-WindowsUpdateDriver
 Start /Wait PowerShell -NoL -C Start-WindowsUpdate
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/SystemPrepScript.ps1
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/Remove-Appx-AllUsers2.ps1
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/Remove-OneDriveSetup_RunKey.ps1
 REM Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
 $AutopilotCMD | Out-File -FilePath 'C:\Windows\System32\Autopilot.cmd' -Encoding ascii -Force
