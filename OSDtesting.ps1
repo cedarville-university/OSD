5cf63a9c47c298ece15580f2e862abc77c70bdda
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2025-02-13 09:34:28 -0500

    Update OSDtesting.ps1

--- OSDtesting.ps1
+++ OSDtesting.ps1
@@ -37,6 +37,7 @@ Start /wait Powershell -NoL -C Set-PSRepository -Name PSGallery -InstallationPol
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose -SkipPublisherCheck
 Start /Wait PowerShell -NoL -C Start-WindowsUpdate
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/SystemPrepScript.ps1
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/cedarville-university/OSD/main/Remove-OneDriveSetup_RunKey.ps1
 REM Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
 $AutopilotCMD | Out-File -FilePath 'C:\Windows\System32\Autopilot.cmd' -Encoding ascii -Force
