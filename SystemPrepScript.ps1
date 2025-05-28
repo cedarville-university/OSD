56733c8ef4b5db2211ecc21c75745dbbe35a3fff
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2025-05-28 14:16:35 -0400

    Update SystemPrepScript.ps1

--- SystemPrepScript.ps1
+++ SystemPrepScript.ps1
@@ -10,10 +10,10 @@ $chassisTypes = (Get-CimInstance -ClassName win32_computersystem).PCSystemType
 # Check the ChassisTypes for specific values indicating system type
 if ($chassisTypes -eq 2) {
     Write-Host "The system is a laptop."
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/cedarville-university/OSD/main/universityBackground_2017-11-17_PC_1366x768.jpg -OutFile c:\temp\universityBackground.jpg
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/cedarville-university/OSD/main/20250527-universityBackground.jpg -OutFile c:\temp\universityBackground.jpg
 }  else {
     Write-Host "The system type is Desktop or VM."
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/cedarville-university/OSD/main/universityBackground_2017-11-17_PC_1920x1080.jpg -OutFile c:\temp\universityBackground.jpg 
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/cedarville-university/OSD/main/20250527-universityBackground.jpg -OutFile c:\temp\universityBackground.jpg 
 }
 
 #Copy background file to required folder and then delete downloaded file from Github.
