872ea78b8ceae84e56c0e5690b2e4c20c325f2aa
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2025-06-30 14:56:03 -0400

    Update OSDInstall.ps1

--- OSDInstall.ps1
+++ OSDInstall.ps1
@@ -1,1 +1,27 @@
+#================================================
+#   [PreOS] Update Module 
+#================================================
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force -SkipPublisherCheck
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force   
+
+#=======================================================================
+#   [OS] Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "24H2"
+    OSEdition = "Education"
+    OSLanguage = "en-us"
+    OSActivation = "Volume"
+    ZTI = $true
+}
+Start-OSDCloud @Params
 
