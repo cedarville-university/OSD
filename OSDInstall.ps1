d9a916f4fd931d1b7156f98d0c1d4f747dcd480b
timoverdorf <71829403+toverdorf@users.noreply.github.com>
2025-06-30 14:57:10 -0400

    Update OSDInstall.ps1

--- OSDInstall.ps1
+++ OSDInstall.ps1
@@ -25,3 +25,19 @@ $Params = @{
 }
 Start-OSDCloud @Params
 
+#================================================
+#  [PostOS] SetupComplete CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+RD C:\OSDCloud /S /Q
+RD C:\Drivers /S /Q
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
